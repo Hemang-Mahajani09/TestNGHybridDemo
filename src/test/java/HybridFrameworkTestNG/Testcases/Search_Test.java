8267210bbf6427affca74e158524fd1a5706b26f
Hemang <76701613+Hemang-Mahajani09@users.noreply.github.com>
2024-07-12 19:07:54 +0530

    Revert "making changes 11 pass"
    This reverts commit 3d21f0bc4aa9506db749fd85f43a838a675258d0.

--- src/test/java/HybridFrameworkTestNG/Testcases/Search_Test.java
+++ src/test/java/HybridFrameworkTestNG/Testcases/Search_Test.java
@@ -8,7 +8,6 @@ import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -50,7 +49,4 @@ public class Search_Test extends BrowserSetup{
 	String actualmessage = driver.findElement(By.xpath("//div[@id='content']/h2/following-sibling::p")).getText();
 	Assert.assertEquals(actualmessage,"There is no product that matches the search criteria.", "Matching criteria failed");
 	}
-	
-	@AfterMethod
-	public void cleanup() {driver.quit();}
 }
