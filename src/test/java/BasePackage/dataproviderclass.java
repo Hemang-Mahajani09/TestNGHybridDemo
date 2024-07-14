9448264b40f56a1591f4a0bf765e24c1a1f9e1cf
hemangmahajani09@gmail.com <76701613+Hemang-Mahajani09@users.noreply.github.com>
2024-07-14 20:54:14 +0530

    debug build

--- src/test/java/BasePackage/dataproviderclass.java
+++ src/test/java/BasePackage/dataproviderclass.java
@@ -7,9 +7,8 @@ public class dataproviderclass {
 	
 	@DataProvider (name = "TestDataforlogin")
 	public String[][] datasupplier() {
-			String [][] data= {
-					{"https://tutorialsninja.com/demo/", "hemangmahajani10@gmail.com", "123456"}	,
-					{"https://tutorialsninja.com/demo/", "hemangmahajani09@gmail.com", "12345",}};
+			String [][] data= {{"hemangmahajani10@gmail.com", "123456"}	,
+					{"hemangmahajani09@gmail.com", "12345",}};
 		
 			return data;
 		}
